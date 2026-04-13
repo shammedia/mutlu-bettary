a2c1154c53e5d2c0936c82dc0abd44abc7f60bcf
Adnan <mh.shamey@gmail.com>
2026-04-13 13:05:40 +0300

    git commit add price

---
+++ Modules/Cart/app/Http/Controllers/CartController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Modules\Cart\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class CartController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+
+        return Inertia::render('Cart::CartIndex');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('cart::create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Show the specified resource.
+     */
+    public function show($id)
+    {
+        return view('cart::show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id)
+    {
+        return view('cart::edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ Modules/Cart/resources/assets/js/Pages/CartIndex.vue
@@ -0,0 +1,16 @@
+<template>
+    <Head>
+        <title>{{ trans("Our Products") }} | Mutlu</title>
+    </Head>
+    <app-layout>
+<h2>Adnan</h2>
+    </app-layout>
+</template>
+<script setup>
+import { computed, ref, watch, onMounted, nextTick } from 'vue'
+import { usePage, Link, router, Head } from '@inertiajs/vue3'
+import AppLayout from '@/Layouts/App.vue'
+</script>
+<style scoped>
+
+</style>
