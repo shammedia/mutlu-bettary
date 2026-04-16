c26430d3434aafc7cb7742ff33efdaacd772e89e
Adnan <mh.shamey@gmail.com>
2026-04-16 11:50:18 +0300

    add orders

--- Modules/Support/routes/web.php
+++ Modules/Support/routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Modules\Support\app\Http\Controllers\ContactUsController;
 
 Route::get('/contact-us', [ContactUsController::class, 'index'])->name('contact-us');
-Route::post('/contact-us', [ContactUsController::class, 'store'])->name('contact-us.Store');
-Route::post('/complaint', [ContactUsController::class, 'storeComplaint'])->name('complaint.Store');
+Route::post('/contact-us', [ContactUsController::class, 'store'])->name('contact-us.store');
+Route::post('/complaint', [ContactUsController::class, 'storeComplaint'])->name('complaint.store');
 Route::post('/subscribe', [ContactUsController::class, 'subscribe'])->name('subscribe');
